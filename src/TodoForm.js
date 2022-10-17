36d6613302367301a87e518b1721b954d35e820f
Anna Bartsytska <anya.bartsitska@gmail.com>
2022-10-17 11:07:51 +0300

    todo

---
+++ src/TodoForm.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react"
+function TodoForm({addTask}) {
+    const [userInput, setUserInput] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addTask(userInput)
+        setUserInput('')
+    }
+    const handleChange = (e) => {    
+        setUserInput(e.currentTarget.value)
+    }
+    const handleKeyPress = (e) => {
+        if(e.key === 'Enter') {
+            handleSubmit(e)
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+            value={userInput}
+            type='text'
+            onChange={handleChange}
+            onKeyDown={handleKeyPress}
+            placeholder='Enter task'
+            />
+            <button>Save</button>
+        </form>
+    );
+}
+export default TodoForm
